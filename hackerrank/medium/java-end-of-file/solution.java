87f46ae29ed81e1adf06d373e070d6ca54bf8b50
Rahul Rawat <rahulrwt7454@gmail.com>
2026-09-02 23:13:19 +0530

    ✅ Java End-of-file | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- hackerrank/medium/java-end-of-file/solution.java
+++ hackerrank/medium/java-end-of-file/solution.java
@@ -1,42 +1,16 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+public class Solution {
 
-
-class Solution{
-    public static void main(String []argh)
-    {
-
-
-
-        Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-
-        for(int i=0;i<t;i++)
-        {
-
-            try
-            {
-                long x=sc.nextLong();
-                System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
-                if(x >= -32768 && x <= 32767)
-    System.out.println("* short");
-
-if(x >= -2147483648L && x <= 2147483647L)
-    System.out.println("* int");
-
-if(x >= Long.MIN_VALUE && x <= Long.MAX_VALUE)
-    System.out.println("* long");
-            }
-            catch(Exception e)
-            {
-                System.out.println(sc.next()+" can't be fitted anywhere.");
-            }
-
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in= new Scanner(System.in);
+        int i=1;
+        while(in.hasNext()){
+            String a= in.nextLine();
+            System.out.println(i + " "+ a);
+            i++;
         }
     }
 }
-
-
-
