2a9f3cafca077e536f0c8c835178c8cdc12f7339
Rahul Rawat <rahulrwt7454@gmail.com>
2026-09-02 23:25:11 +0530

    ✅ Java Static Initializer Block | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- hackerrank/medium/java-static-initializer-block/solution.java
+++ hackerrank/medium/java-static-initializer-block/solution.java
@@ -5,12 +5,21 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner in= new Scanner(System.in);
-        int i=1;
-        while(in.hasNext()){
-            String a= in.nextLine();
-            System.out.println(i + " "+ a);
-            i++;
-        }
+        Scanner input = new Scanner(System.in);
+        int a=input.nextInt();
+        int b=input.nextInt();
+        try {
+    if(a <= 0 || b <= 0) {
+        throw new Exception("Breadth and height must be positive");
     }
+
+    int area = a * b;
+    System.out.println(area);
 }
+catch(Exception e) {
+    System.out.println(e);
+}
+        }
+        
+        }
+    
