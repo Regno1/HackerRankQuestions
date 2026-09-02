20c20f305b128a117463cd58ec446dded14c3125
Rahul Rawat <rahulrwt7454@gmail.com>
2026-09-02 19:25:23 +0530

    ✅ Java Stdin and Stdout II | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- hackerrank/medium/java-stdin-stdout/solution.java
+++ hackerrank/medium/java-stdin-stdout/solution.java
@@ -1,37 +1,17 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
 
-
-
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        scanner.close();
-        if(n%2!=0){
-        System.out.println("Weird");
-        }
-        else if(n%2==0){
-        if(2<=n && n<=5){
-        System.out.println("Not Weird");
-        }
-        else if(n>=6 && n<=20){
-        System.out.println("Weird");
-        }
-        else if(n>20){
-        System.out.println("Not Weird");
-        }
-        }
-        
-        
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double d= scan.nextDouble();
+        scan.nextLine();
+        String s= scan.nextLine();
+        scan.close();
+         
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 }
