a35a0da3e43b5200a9f5b6b6e1d84036ad414f86
Rahul Rawat <rahulrwt7454@gmail.com>
2026-09-02 19:15:05 +0530

    ✅ Java If-Else | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- hackerrank/medium/java-if-else/solution.java
+++ hackerrank/medium/java-if-else/solution.java
@@ -21,10 +21,10 @@ public class Solution {
         System.out.println("Weird");
         }
         else if(n%2==0){
-        if(2<=n || n<=5){
+        if(2<=n && n<=5){
         System.out.println("Not Weird");
         }
-        else if(n>=6 || n<=20){
+        else if(n>=6 && n<=20){
         System.out.println("Weird");
         }
         else if(n>20){
