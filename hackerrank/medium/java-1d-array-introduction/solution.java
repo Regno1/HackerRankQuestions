8a23fda7e7c3b52d03e42bf0dfec069f4b4bcb23
Rahul Rawat <rahulrwt7454@gmail.com>
2026-09-04 12:31:06 +0530

    ✅ Java 1D Array | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

--- hackerrank/medium/java-1d-array-introduction/solution.java
+++ hackerrank/medium/java-1d-array-introduction/solution.java
@@ -1,31 +1,6 @@
-import java.util.*;
-import java.text.*;
 
-public class Solution {
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double payment = scanner.nextDouble();
-        scanner.close();
-       
-       
-        // Write your code here.
-        
-  
-  NumberFormat us= NumberFormat.getCurrencyInstance(Locale.US);
-  NumberFormat india= NumberFormat.getCurrencyInstance(new Locale("en","IN"));
-  NumberFormat china= NumberFormat.getCurrencyInstance(Locale.CHINA);
-  NumberFormat france= NumberFormat.getCurrencyInstance(Locale.FRANCE);
 
-        System.out.println("US: " + us.format(payment));
-        System.out.println("India: " + india.format(payment));
-        System.out.println("China: " + china.format(payment));
-        System.out.println("France: " + france.format(payment));
-    }
-    static String formatPayment(double s){
-            NumberFormat nf = NumberFormat.getCurrencyInstance();         
-           
-            return nf.format(s);    
-        }
-   
+int[] a=new int[n];
+for(int i=0;i<n;i++){
+    a[i]=scan.nextInt();
 }
