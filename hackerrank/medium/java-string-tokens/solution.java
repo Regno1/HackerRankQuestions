e27602ce05a619708e3eda98853b9070a1694b1e
Rahul Rawat <rahulrwt7454@gmail.com>
2026-09-06 21:00:21 +0530

    ✅ Java String Tokens | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-06

--- hackerrank/medium/java-string-tokens/solution.java
+++ hackerrank/medium/java-string-tokens/solution.java
@@ -6,9 +6,11 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
+        s=s.replaceAll("\\s+", " ").trim();
         // Write your code here.
-        String[] a= s.split("[\\s+!,?._'@]+");
-        System.out.println(a.length);
+        String[] a= s.split("[\\s!,?._'@]+");
+        if(s.isEmpty()) System.out.print(0);
+        else System.out.println(a.length);
         for(String i:a){
             
             System.out.println(i);
