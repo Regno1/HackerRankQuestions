94cb3c4ddb9f41277003549f6ae85f7fef04a3a6
Rahul Rawat <rahulrwt7454@gmail.com>
2026-09-06 20:56:06 +0530

    ✅ Java String Tokens | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-06

--- hackerrank/medium/java-string-tokens/solution.java
+++ hackerrank/medium/java-string-tokens/solution.java
@@ -1,25 +1,19 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
 
-    static boolean isAnagram(String x, String y) {
-        int[] a=new int[26];
-        int[] b=new int[26];
-        x= x.toLowerCase();
-        y=y.toLowerCase();
-        boolean m=false;
-        if(x.length()!=y.length()) return false;
-        for(char ch:x.toCharArray()){
-            a[ch-'a']++;
-        }
-         for(char ch:y.toCharArray()){
-            b[ch-'a']++;
-        }
-        for(int i=0;i<a.length;i++){
-            if(a[i]!=b[i])  m=true;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        String[] a= s.split("[\\s+!,?._'@]+");
+        System.out.println(a.length);
+        for(String i:a){
             
+            System.out.println(i);
         }
-        if(m){
-            return false;
-        }
-        return true;
+        scan.close();
     }
+}
 
