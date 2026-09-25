44c1b94a4a2f00a6d4dff4c6bade93b6d607eeaa
Rahul Rawat <rahulrwt7454@gmail.com>
2026-09-25 11:03:37 +0530

    ✅ Factorial | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ gfg/basic/factorial5739/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    int factorial(int n) {
+        // code here
+        int sum=1;
+        for(int i=1;i<=n;i++){
+            sum=sum*i;
+        }
+        
+        return sum;
+    }
+}
